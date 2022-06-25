697b4fb6b813cc2c3169db0a5735bab4215c489a
MLEFTII <kamausylvester897@gmail.com>
2022-06-25 12:44:23 +0300

    first commit

--- i4g014345q0h/blog/views.py
+++ i4g014345q0h/blog/views.py
@@ -1,25 +1,32 @@
 from dataclasses import fields
 from pyexpat import model
 from django.shortcuts import render
-from .models import Post
-# from django.contrib.auth import reverse_lazy
+import blog
+from blog.models import Post
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+
 
 # Create your views here.
 
 
-class PostListView():
+class PostListView(ListView):
     model=Post
-class PostCreateView():
+class PostCreateView(CreateView):
     model=Post
     fields="__all__"
     success_url=reverse_lazy("blog:all")
-class PostDetailView():
+class PostDetailView(DetailView):
     model = Post
-class PostUpdateView():
+class PostUpdateView(UpdateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
-class PostDeleteView():
+    fields="__all__"
+    success_url=reverse_lazy("blog:all")
+class PostDeleteView(DeleteView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields="__all__"
+    success_url=reverse_lazy("blog:all")
